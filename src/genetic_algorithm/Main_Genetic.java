a5083964f969898ef81e7a0a00e1551e1c4508ab
paolocurotto <curotto2@hotmail.com>
2018-11-18 21:39:09 -0300

    genetic algorithm

---
+++ src/genetic_algorithm/Main_Genetic.java
@@ -0,0 +1,9 @@
+package genetic_algorithm;
+
+public class Main_Genetic {
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/genetic_algorithm/WordSequence.java
+++ src/genetic_algorithm/WordSequence.java
@@ -1,7 +1,9 @@
 package genetic_algorithm;
 
+import javax.sound.sampled.Line;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 public class WordSequence {
 
@@ -15,15 +17,12 @@ public class WordSequence {
         String secretWord = "GENETICALGORITHM"; // Secret word
         int n_letters = secretWord.length(); // Secret word's number of letters
         int generationCounter = 0; // To count generations
-
-        List<Individual> population = new ArrayList<>();
-        List<Individual> new_generation = new ArrayList<>();
-        List<Individual> temp;
+        List<Individual> population = new ArrayList<>(); // Population's individuals list
+        List<Individual> new_generation = new ArrayList<>(); // Population's offspring generation
+        int k = 30; // For tournament selection
 
         // Initialize population
-        for (int i = 0; i < N; i++) {
-            population.add(new Individual(n_letters));
-        }
+        IntStream.range(0, N).forEach(i -> population.add(new Individual(n_letters)));
 
         // Evaluate fitness
         population.forEach(individual -> individual.calculateFitness(secretWord));
@@ -39,24 +38,48 @@ public class WordSequence {
         /** Genetic algorithm **/
         while (solutionFound == false) {
 
+
             // Selection
+            /*
             int selectionAmount = N / 5;
             List<Individual> bestIndividuals = population.subList(0, selectionAmount);
 
             // Reproduction
             for (int i = 0; i < N; i++) {
-                Individual m = bestIndividuals.get(ThreadLocalRandom.current().nextInt(0, selectionAmount));
-                Individual f = bestIndividuals.get(ThreadLocalRandom.current().nextInt(0, selectionAmount));
-                while (m.equals(f) == true) {
-                    f = population.get(ThreadLocalRandom.current().nextInt(0, selectionAmount));
-                }
-
-                Individual new_individual = m.breed(f);
-                new_generation.add(new_individual);
+                int father_index = ThreadLocalRandom.current().nextInt(0, selectionAmount);
+                int mother_index = (father_index + ThreadLocalRandom.current().nextInt(1, selectionAmount)) % selectionAmount;
+                Individual father = bestIndividuals.get(father_index);
+                Individual mother = bestIndividuals.get(mother_index);
+                Individual baby = father.breed(mother);
+                new_generation.add(baby);
             }
-            temp = population;
-            population = new_generation;
-            new_generation = temp;
+            */
+
+
+            // Tournament selection and reproduction
+            for (int n = 0; n < N; n++) {
+                int father_index = -1; // Find dad
+                for (int i = 0; i < k; i++) {
+                    int best_index = ThreadLocalRandom.current().nextInt(0, N);
+                    if (father_index == -1 || population.get(best_index).fitness > population.get(father_index).fitness) {
+                        father_index = best_index;
+                    }
+                }
+                int mother_index = -1; // Find mom
+                for (int i = 0; i < k; i++) {
+                    int best_index = (father_index + ThreadLocalRandom.current().nextInt(1, N)) % N;
+                    if (i == 0 || population.get(best_index).fitness > population.get(mother_index).fitness) {
+                        mother_index = best_index;
+                    }
+                }
+                Individual baby = population.get(father_index).breed(population.get(mother_index));
+                new_generation.add(baby);
+            } // Tournament selection end
+
+
+            // Set offspring as the current generation and kill old generation
+            population.clear();
+            population.addAll(new_generation);
             new_generation.clear();
 
             // Evaluate fitness
@@ -69,25 +92,20 @@ public class WordSequence {
             // Print info
             print_info(++generationCounter, population);
         }
-        System.out.println("Finished.");
     }
 
     static void print_info(int c, List<Individual> pop) {
+        String genes = pop.get(0).genes;
         System.out.print("Generation: " + c + ", best: [");
-        for (int i = 0; i < pop.get(0).genes.length() - 1; i++) {
-            System.out.print(pop.get(0).genes.charAt(i) + ", ");
-        }
-        System.out.print(pop.get(0).genes.charAt(pop.get(0).genes.length() - 1) + "]");
-        System.out.println(", fit/best: (" + pop.get(0).fitness + "/" + pop.get(0).genes.length() + ")");
+        genes.chars().mapToObj(i -> (char) i).forEach(System.out::print);
+        System.out.println("], best/target: (" + pop.get(0).fitness + "/" + genes.length() + ")");
     }
-
 
 
 
     /**
      * Individual
      * */
-    //static class Individual implements Comparable<Individual> {
     static class Individual {
         String genes = "";
         int fitness = 0;
@@ -96,9 +114,7 @@ public class WordSequence {
         }
 
         private Individual(int nletters) {
-            for (int i = 0; i < nletters; i++) {
-                genes = genes + Letter.randomLetter();
-            }
+            IntStream.range(0, nletters).forEach(i -> genes = genes + Letter.randomLetter() );
         }
 
         void calculateFitness(String word_target) {
@@ -119,7 +135,7 @@ public class WordSequence {
                 if (Math.random() < 0.1) {
                     child.genes = child.genes + Letter.randomLetter();
                 }
-                // Pick father gene, 50% change
+                // Pick father gene, 50% chance
                 else if (Math.random() < 0.5) {
                     child.genes = child.genes + gene_x.charAt(i);
                 }
